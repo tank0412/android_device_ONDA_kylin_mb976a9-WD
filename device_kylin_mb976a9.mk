834263a26c0b125d80365ac73994352c7a24776d
Roman <romabukhtiarov@gmail.com>
2015-09-01 06:20:00 +0300

    Minor fixes for custom boot.img

--- device_kylin_mb976a9.mk
+++ device_kylin_mb976a9.mk
@@ -157,7 +157,7 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/recovery/init.sun9i.usb.rc:root/init.sun9i.usb.rc
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/recovery/init_partition.sh:recovery/root/sbin/init_partition.sh
+	$(LOCAL_PATH)/recovery/init_partition.sh:root/sbin/init_partition.sh
 
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/recovery/Extra/file_contexts:root/file_contexts
